7ea43f63f9c0205723dfa1005cfa358519eb8706
Alice Di Nunno <alice@dinunno.fr>
2020-01-19 22:15:17 +0100

    Added sqlite driver

--- src/github.com/AliceDiNunno/transtracker-api/API.go
+++ src/github.com/AliceDiNunno/transtracker-api/API.go
@@ -16,10 +16,5 @@ func StartApi() {
 		panic("failed to connect database: " + err.Error())
 	}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	r.Run(":30100") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
